8490d0e70ac66a4042edb35ee4680e97dc40d7df
Ritesh Sharma <riteshbbn74@gmail.com>
2024-08-22 15:04:27 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,8 +4,6 @@ import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
 import { validationResult } from 'express-validator';
 import { JwtPayload } from 'jsonwebtoken';
-import { AppDataSource } from '../config/data-source';
-import { RefreshToken } from '../entity/RefreshToken';
 import { TokenService } from '../services/TokenService';
 
 //You can use Functional Based here class based component bcoz grouping is possible easily
@@ -15,8 +13,9 @@ export class AuthController {
         private logger: Logger,
         private tokenService: TokenService,
     ) {
-        this.userService = userService;
-        this.tokenService = tokenService;
+        //Automatically assigned that's why commented
+        // this.userService = userService;
+        // this.tokenService = tokenService;
         // this.register = this.register.bind(this);
     }
 
@@ -67,23 +66,9 @@ export class AuthController {
 
             const accessToken = this.tokenService.generateAccessToken(payload);
 
-            //Persist the refresh Token
-            const MS_IN_YEAR = 100 * 60 * 60 * 24 * 365; //1y-> (Leap year)
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
 
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
-            //Payload can be different
-            //secret key must be strong , isko ham get karenge .env se
-            // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-            //     algorithm: 'HS256',
-            //     expiresIn: '1y',
-            //     issuer: 'auth-service',
-            //     jwtid: String(newRefreshToken.id),
-            // });
             const refreshToken = this.tokenService.generateRefreshToken({
                 ...payload,
                 id: String(newRefreshToken.id),

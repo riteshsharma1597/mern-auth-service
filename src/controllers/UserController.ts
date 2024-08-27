12f1f2700abc3751c25a848f7267ea9c77af9f8b
Ritesh Sharma <riteshbbn74@gmail.com>
2024-08-28 03:40:46 +0530

    validation create user added

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserService } from '../services/UserService';
 import { Roles } from '../constants';
+import { validationResult } from 'express-validator';
 
 export class UserController {
     constructor(private userService: UserService) {}
     async create(req: Request, res: Response, next: NextFunction) {
+        //Validation
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+
         const { firstName, lastName, email, password } = req.body;
         try {
             const user = await this.userService.create({
